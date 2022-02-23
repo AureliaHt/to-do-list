28e63f18037b8e324eacaa4bf53a82a341121bae
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-02-23 14:51:01 +0100

    mise en place du pomodoro timer, des boutons et ajout de fichiers

---
+++ js/pomodoro.js
@@ -0,0 +1,14 @@
+const timer = document.getElementById("timer");
+const timerControl = document.getElementById("toggle");
+const pomodoroTimer = document.querySelector("[class=pomodoroSelector]");
+const pomodoroButton = document.getElementById("pomodoro_button");
+
+let seconds = 0;
+
+const timeTimer = {
+    pomodoro = 25
+}
+
+
+
+
