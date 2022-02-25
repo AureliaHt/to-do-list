e6757376ba673495785a3381467da073f9847d69
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-02-25 17:01:13 +0100

    javascript pour le timer du pomodoro

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,54 @@
 const timer = document.getElementById("timer");
-const timerControl = document.getElementById("toggle");
-const pomodoroTimer = document.querySelector("[class=pomodoroSelector]");
+const modeButtons = document.querySelector("[class=modeSelector]");
 const pomodoroButton = document.getElementById("pomodoro_button");
+const shortButton = document.getElementById("short_button");
+const longButton = document.getElementById("long_button");
+const mainButton = document.getElementById("toggle");
 
 let seconds = 0;
 
-const timeTimer = {
-    pomodoro = 25
+const TIMER = {
+    POMODORO = 25,
+    SHORTBREAK = 5,
+    LONGBREAK = 15
+};
+
+function pomodoro() {
+    function setTimer() {
+        if (timer.dataset.mode == "pomodoro") {
+            seconds = TIMER.POMODORO * 60;
+        }else if(timer.dataset.mode == "short") {
+            seconds = TIMER.SHORTBREAK * 60;
+        }else{
+            seconds = TIMER.LONGBREAK * 60;
+        }
+    }
+
+    if (mainButton.classList.contains("fa-play")) {
+        mainButton.classList.replace("fa-play", "fa-stop");
+        mainButton.dataset.paused = "false";
+
+        setTimer();
+
+        interval = setInterval(() => {
+            let timeRemaining = 
+            ("0" + Math.floor(seconds / 60)).slice(-2) + 
+            ":" +
+             ("0" + (seconds % 60)).slice(-2);
+             timer.innerHTML = timeRemaining;
+             document.title = `${timeRemaining} - ${
+                 timer.dataset.mode == "pomodoro" ? "Work" : "Break"
+                }`;
+
+        if (mainButton.dataset.paused == "true" || seconds == 0){
+            clearInterval(interval);
+        }
+        seconds--;
+        }, 1000);
+    }else{
+        mainButton.classList.replace("fa-stop", "fa-play");
+        mainButton.dataset.paused = "true";
+    }
 }
 
-
-
-
+mainButton.addEventListener("click", pomodoro);
